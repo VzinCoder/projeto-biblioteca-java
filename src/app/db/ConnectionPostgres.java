55e9b6201115ace02f7d51184f292ee4ffa420e6
VzinCoder <contato.viniciusp.b@gmail.com>
2024-05-08 22:17:21 -0300

    classe para conexao com banco de dados

---
+++ src/app/db/ConnectionPostgres.java
@@ -0,0 +1,25 @@
+package app.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionPostgres {
+    private static final String URL = "jdbc:postgresql://localhost/biblioteca";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "12345678";
+
+
+    public static Connection createConnection() throws ClassNotFoundException, SQLException{
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return conn;
+    }
+   
+
+    public static void closeConnection(Connection conn) throws SQLException {
+       conn.close();
+    }
+
+
+}
