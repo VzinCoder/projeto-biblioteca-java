7ee411e2d55c795ac99c47612d830d70228f0080
VzinCoder <contato.viniciusp.b@gmail.com>
2024-05-09 11:48:29 -0300

    editando e finalizando testes da classse UsuarioRepository

--- src/app/repository/UsuarioRepository.java
+++ src/app/repository/UsuarioRepository.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class UsuarioRepository {
@@ -16,33 +18,58 @@ public class UsuarioRepository {
         this.conn = ConnectionPostgres.createConnection();
     }
 
-    public void adicionarUsuario(Usuario usuario) throws SQLException, AdicionaUsuarioException {
+    public int adicionarUsuario(Usuario usuario) throws SQLException, AdicionaUsuarioException {
         Optional<Usuario> usuarioEncontrado = buscaUsuario(usuario.getCpf());
 
-        if(!usuario.validaCpf()){
+        if (!usuario.validaCpf()) {
             throw new AdicionaUsuarioException("Cpf Invalido!");
         }
 
-        if(usuarioEncontrado.isPresent()){
+        if (usuarioEncontrado.isPresent()) {
             throw new AdicionaUsuarioException("Já existe um usuario com esse cpf!");
         }
 
         String sql = "Insert into usuarios(nome,cpf) values (?,?);";
 
-        PreparedStatement pst = conn.prepareStatement(sql);
+        PreparedStatement pst = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
         pst.setString(1, usuario.getNome());
         pst.setString(2, usuario.getCpf());
         pst.executeUpdate();
+        ResultSet resultSet = pst.getGeneratedKeys();
+        resultSet.next();
+        return resultSet.getInt("id");
     }
 
     public void removerUsuario(String cpf) throws SQLException, RemoveUsuarioException {
+        boolean cpfValido = Usuario.validarCpfUsuario(cpf);
+
+        if (!cpfValido) {
+            throw new RemoveUsuarioException("Formato de Cpf Invalido!");
+        }
+
         String sql = "Delete from  usuarios where cpf=?";
         PreparedStatement pst = conn.prepareStatement(sql);
         pst.setString(1, cpf);
         int result = pst.executeUpdate();
 
         if (result == 0) {
-            throw new RemoveUsuarioException("Usuario nao encontrado");
+            throw new RemoveUsuarioException("Usuario nao encontrado!");
+        }
+    }
+
+    public void removerUsuarioPorId(int id) throws SQLException, RemoveUsuarioException {
+
+        if (id < 1) {
+            throw new RemoveUsuarioException("id Invalido!");
+        }
+
+        String sql = "Delete from  usuarios where id=?";
+        PreparedStatement pst = conn.prepareStatement(sql);
+        pst.setInt(1, id);
+        int result = pst.executeUpdate();
+
+        if (result == 0) {
+            throw new RemoveUsuarioException("Usuario nao encontrado!");
         }
     }
 
@@ -55,10 +82,68 @@ public class UsuarioRepository {
         Optional<Usuario> usuario = Optional.empty();
         if (result.next()) {
             String nomeUsuario = result.getString("nome");
-            usuario = Optional.of(new Usuario(nomeUsuario, cpf));
+            int idUsuario = result.getInt("id");
+            usuario = Optional.of(new Usuario(idUsuario, nomeUsuario, cpf));
         }
 
         return usuario;
+    }
+
+    public List<Usuario> buscarUsuariosPorNome(String nome) throws SQLException {
+        List<Usuario> usuariosEncontrados = new ArrayList<>();
+        String sql = "SELECT * FROM usuarios WHERE nome LIKE ?";
+
+        PreparedStatement pst = conn.prepareStatement(sql);
+        pst.setString(1, "%" + nome + "%");
+
+        ResultSet resultSet = pst.executeQuery();
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String nomeUsuario = resultSet.getString("nome");
+            String cpf = resultSet.getString("cpf");
+            Usuario usuario = new Usuario(id, nomeUsuario, cpf);
+            usuariosEncontrados.add(usuario);
+
+        }
+
+        return usuariosEncontrados;
+    }
+
+    public void atualizarUsuario(String cpf, Usuario usuarioAtualizado) throws AtualizarUsuarioException, SQLException {
+        Optional<Usuario> usuarioEncontrado = buscaUsuario(cpf);
+        boolean cpfValido = Usuario.validarCpfUsuario(cpf);
+        Optional<Usuario> usuarioCpfUso = buscaUsuario(usuarioAtualizado.getCpf());
+
+        if (!cpfValido) {
+            throw new AtualizarUsuarioException("Formato de Cpf Invalido!");
+        }
+
+        if (!usuarioAtualizado.validaCpf()) {
+            throw new AtualizarUsuarioException("Novo Cpf com formato Invalido!");
+        }
+
+        if (usuarioEncontrado.isEmpty()) {
+            throw new AtualizarUsuarioException("Usuario nao encontrado!");
+        }
+
+        if (usuarioCpfUso.isPresent() && verificaCpfUso(usuarioEncontrado.get(), usuarioCpfUso.get())) {
+            throw new AtualizarUsuarioException("Cpf em Uso!");
+        }
+
+        String sql = "Update usuarios set nome=?, cpf=? where cpf=?;";
+        PreparedStatement pst = conn.prepareStatement(sql);
+        pst.setString(1, usuarioAtualizado.getNome());
+        pst.setString(2, usuarioAtualizado.getCpf());
+        pst.setString(3, cpf);
+        pst.executeUpdate();
+
+    }
+
+    private boolean verificaCpfUso(Usuario atual, Usuario encontrado) {
+        if (atual.getId() != encontrado.getId()) {
+            return true;
+        }
+        return false;
     }
 
     public void closeConnection() throws SQLException {
--- src/tests/repository/UsuarioRepositoryTest.java
+++ src/tests/repository/UsuarioRepositoryTest.java
@@ -1,6 +1,8 @@
 package tests.repository;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -9,6 +11,7 @@ import app.customexpections.*;
 import app.model.Usuario;
 import app.repository.UsuarioRepository;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.After;
@@ -23,19 +26,22 @@ public class UsuarioRepositoryTest {
     @Before
     public void iniciarConexao() {
         try {
+            String nome = "vinicius";
+            String cpf = "12345678901";
             usuarioRepository = new UsuarioRepository();
-            usuario = new Usuario("vinicius", "12345678901");
-            usuarioRepository.adicionarUsuario(usuario);
+            usuario = new Usuario(nome, cpf);
+            int id = usuarioRepository.adicionarUsuario(usuario);
+            usuario.setId(id);
         } catch (SQLException | AdicionaUsuarioException | ClassNotFoundException e) {
             fail(e.getMessage());
         }
-       
+
     }
 
     @Test
-    public void buscaUsuarioTest(){
+    public void buscaUsuarioTest() {
         String cpfinvalido = "123";
-        
+
         try {
             // testa busca com cpf invalido
             assertTrue(usuarioRepository.buscaUsuario(cpfinvalido).isEmpty());
@@ -45,7 +51,7 @@ public class UsuarioRepositoryTest {
             assertEquals(usuario.getCpf(), usuarioEncontrado.get().getCpf());
             assertEquals(usuario.getNome(), usuarioEncontrado.get().getNome());
 
-        } catch (Exception e) {
+        } catch (SQLException e) {
             fail(e.getMessage());
         }
 
@@ -55,26 +61,120 @@ public class UsuarioRepositoryTest {
     public void adicionarUsuarioTest() {
         // adicionando usuario com cpf menor que 12
         Usuario usuarioInvalido = new Usuario("teste", "123");
-        
-        assertThrows(AdicionaUsuarioException.class, ()-> usuarioRepository.adicionarUsuario(usuarioInvalido));
+
+        assertThrows(AdicionaUsuarioException.class, () -> usuarioRepository.adicionarUsuario(usuarioInvalido));
 
         // adicionando usuario com cpf do tamanho correto porem invalido
         usuarioInvalido.setCpf("23456vinici");
-        assertThrows(AdicionaUsuarioException.class, ()-> usuarioRepository.adicionarUsuario(usuarioInvalido));
+        assertThrows(AdicionaUsuarioException.class, () -> usuarioRepository.adicionarUsuario(usuarioInvalido));
 
         // adicionando usuario com cpf já existente
         usuarioInvalido.setCpf("12345678901");
-        assertThrows(AdicionaUsuarioException.class, ()-> usuarioRepository.adicionarUsuario(usuarioInvalido));
+        assertThrows(AdicionaUsuarioException.class, () -> usuarioRepository.adicionarUsuario(usuarioInvalido));
+    }
+
+    @Test
+    public void removerUsuarioTest() {
+
+        // remover usuario com formato de cpf invalido
+        assertThrows(RemoveUsuarioException.class, () -> usuarioRepository.removerUsuario("123z"));
+
+        // remover usuario que nao existe
+        assertThrows(RemoveUsuarioException.class, () -> usuarioRepository.removerUsuario("22222222222"));
+    }
+
+    @Test
+    public void atualizarUsuarioTest() {
+        Usuario usuarioTest = new Usuario("test", "invalid cpf");
+        String cpfFormatValid = "22222222222";
+        String cpfInvalid = "143vhga";
+
+        // buscando com cpf invalido
+        assertThrows(AtualizarUsuarioException.class,
+                () -> usuarioRepository.atualizarUsuario(cpfInvalid, usuarioTest));
+
+        // atualizando usuario que nao existe
+        assertThrows(AtualizarUsuarioException.class,
+                () -> usuarioRepository.atualizarUsuario(cpfFormatValid, usuarioTest));
+
+        // atualizando usuario com novo cpf invalido
+        assertThrows(AtualizarUsuarioException.class,
+                () -> usuarioRepository.atualizarUsuario(usuario.getCpf(), usuarioTest));
+
+        // atualizando cpf do usuario para um já existente
+        try {
+            Usuario usuarioExistente = new Usuario("usuario test", cpfFormatValid);
+            usuarioRepository.adicionarUsuario(usuarioExistente);
+            assertThrows(AtualizarUsuarioException.class,
+                    () -> usuarioRepository.atualizarUsuario(usuario.getCpf(), usuarioExistente));
+            usuarioRepository.removerUsuario(cpfFormatValid);
+        } catch (SQLException | RemoveUsuarioException | AdicionaUsuarioException e) {
+            fail(e.getMessage());
+        }
+
+        // atualizando nome de um usuario existente
+        try {
+            // atualizando dados do usuario test
+            String nomeAntigo = usuario.getNome();
+            usuario.setNome("novo nome test");
+
+            usuarioRepository.atualizarUsuario(usuario.getCpf(), usuario);
+            Optional<Usuario> usuarioEncontrado = usuarioRepository.buscaUsuario(usuario.getCpf());
+            assertNotEquals(nomeAntigo, usuarioEncontrado.get().getNome());
+            assertEquals(usuario.getNome(), usuarioEncontrado.get().getNome());
+            assertEquals(usuario.getCpf(), usuarioEncontrado.get().getCpf());
+
+        } catch (SQLException | AtualizarUsuarioException e) {
+            fail(e.getMessage());
+        }
+
+        // atualizando nome e cpf de um usuario existente
+        try {
+            String nomeAntigo = usuario.getNome();
+            usuarioTest.setNome("novo nome test2");
+            usuarioTest.setCpf(cpfFormatValid);
+
+            usuarioRepository.atualizarUsuario(usuario.getCpf(), usuarioTest);
+            Optional<Usuario> usuarioEncontrado = usuarioRepository.buscaUsuario(usuarioTest.getCpf());
+            assertNotEquals(nomeAntigo, usuarioEncontrado.get().getNome());
+            assertEquals(usuarioTest.getNome(), usuarioEncontrado.get().getNome());
+            assertEquals(usuarioTest.getCpf(), usuarioEncontrado.get().getCpf());
+
+        } catch (SQLException | AtualizarUsuarioException e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void buscarUsuariosPorNomeTest() {
+        try {
+            // buscando usuarios existente
+            List<Usuario> usuarios = usuarioRepository.buscarUsuariosPorNome("vinicius");
+            assertFalse(usuarios.isEmpty());
+
+            // buscando usuarios existente usando apenas uma parte do nome
+            usuarios = usuarioRepository.buscarUsuariosPorNome("nici");
+            assertFalse(usuarios.isEmpty());
+
+            // buscando usuarios que nao existem
+            usuarios = usuarioRepository.buscarUsuariosPorNome("ahfdaui");
+            assertTrue(usuarios.isEmpty());
+        } catch (SQLException e) {
+            fail(e.getMessage());
+        }
+
     }
 
     @After
     public void fecharConexao() {
         try {
-            usuarioRepository.removerUsuario(usuario.getCpf());
+            usuarioRepository.removerUsuarioPorId(usuario.getId());
+
             usuarioRepository.closeConnection();
         } catch (SQLException | RemoveUsuarioException e) {
             fail(e.getMessage());
         }
-      
+
     }
 }
